e234448c5cbcb6467f9af27cd2c99f607ced432e
Desmond Good <desmondjgood@gmail.com>
2025-05-03 22:03:49 -0700

    Add some BQ25798 commands for proper system charge voltage

--- src/main.c
+++ src/main.c
@@ -185,9 +185,183 @@ int enable_charging(void)
 	return ret;
 }
 
-int program_charge_params()
+#define REG_Minimum_System_Voltage      0x00
+#define REG_Charge_Voltage_Limit        0x01
+#define REG_Charge_Current_Limit        0x03
+#define REG_Input_Voltage_Limit         0x05
+#define REG_Input_Current_Limit         0x06
+#define REG_Charger_Control_0           0x0F
+#define REG_Precharge_Control           0x08
+#define REG_Termination_Control         0x09
+#define REG_Charger_Control_1           0x10
+#define REG_Charger_Control_5           0x14
+#define REG_MPPT_Control               	0x15
+#define REG_Charger_Status_0            0x1B
+#define REG_Charger_Status_1            0x1C
+#define REG_Charger_Status_2            0x1D
+#define REG_Charger_Status_3            0x1E
+#define REG_Charger_Status_4            0x1F
+#define REG_Charger_Flag_0              0x22
+#define REG_ADC_Control             	0x2E
+#define REG_ADC_Function_Disable_1	    0x30
+
+static int i2c_reg_write_word_dt(const struct i2c_dt_spec *i2c, uint8_t reg_addr, uint16_t value)
 {
+    uint8_t bytes[3];
+
+    bytes[0] = reg_addr;
+    bytes[1] = (uint8_t)(value >> 8);
+    bytes[2] = (uint8_t)(value & 0xFF);
+
+    return i2c_write_dt(i2c, bytes, sizeof(bytes));
+}
+
+static int i2c_reg_read_word_dt(const struct i2c_dt_spec * i2c, uint8_t reg_addr, uint16_t * value)
+{
+    uint8_t buf[2];
+    int ret = i2c_write_read_dt(i2c, &reg_addr, 1, buf, 2);
+    if (ret != 0) {
+        return ret;
+    }
+
+    *value = ((uint16_t)buf[0] << 8) | buf[1];
+    return 0;
+}
+
+int program_charge_params(void)
+{
+	// reset
+	if (i2c_reg_write_byte_dt(&pmic_i2c, REG_Termination_Control, 0x40)) {
+		goto failed;
+	}	
+
+	k_sleep(K_MSEC(20));
+
+	// bq.disable_watchdog()
+	if (i2c_reg_write_byte_dt(&pmic_i2c, REG_Charger_Control_1, 0x80)) {
+		goto failed;
+	}
+
+    // bq.config_vsysmin(2500)
+	// 0 * 250 mV + 2500 mV == 2500 mV
+	if (i2c_reg_write_byte_dt(&pmic_i2c, REG_Minimum_System_Voltage, 1)) {
+		goto failed;
+	}
+
+	uint8_t byte_val;
+	if (i2c_reg_read_byte_dt(&pmic_i2c, REG_Minimum_System_Voltage, &byte_val)) {
+		goto failed;
+	}
+	printk("Minimum System Voltage = %d mV\n", (byte_val * 250) + 2500);
+
+	uint16_t word_val;
+    // bq.config_charge_voltage_limit(3500)
+	if (i2c_reg_write_word_dt(&pmic_i2c, REG_Charge_Voltage_Limit, 350)) {
+		goto failed;
+	}
+	
+	if (i2c_reg_read_word_dt(&pmic_i2c, REG_Charge_Voltage_Limit, &word_val)) {
+		goto failed;
+	}
+
+	printk("Charge Voltage Limit = %d mV\n", word_val * 10);
+
+    // bq.config_charge_current_limit(500)
+	if (i2c_reg_write_word_dt(&pmic_i2c, REG_Charge_Current_Limit, 50)) {
+		goto failed;
+	}
+	
+	if (i2c_reg_read_word_dt(&pmic_i2c, REG_Charge_Current_Limit, &word_val)) {
+		goto failed;
+	}
+
+	printk("Charge Current Limit = %d mA\n", word_val * 10);
+
+    // bq.config_precharge_current(40)
+	if (i2c_reg_write_byte_dt(&pmic_i2c, REG_Precharge_Control, 1)) {
+		goto failed;
+	}
+	
+	if (i2c_reg_read_byte_dt(&pmic_i2c, REG_Precharge_Control, &byte_val)) {
+		goto failed;
+	}
+
+	printk("Precharge Current Limit = %d mA\n", byte_val * 40);
+
+    // bq.config_charge_term_current(40)
+	if (i2c_reg_write_byte_dt(&pmic_i2c, REG_Termination_Control, 2)) {
+		goto failed;
+	}
+	
+	if (i2c_reg_read_byte_dt(&pmic_i2c, REG_Termination_Control, &byte_val)) {
+		goto failed;
+	}
+
+	printk("Termination Current Limit = %d mA\n", byte_val * 40);
+
+	// re-run poor source qualification
+    // bq.recheck_vbus()
+	if (i2c_reg_read_byte_dt(&pmic_i2c, REG_Charger_Control_0, &byte_val)) {
+		goto failed;
+	}
+
+	// Force EN_HIZ
+	// byte_val &= ~(1 << 2);
+
+	// if (i2c_reg_write_byte_dt(&pmic_i2c, REG_Charger_Control_0, byte_val)) {
+	// 	goto failed;
+	// }
+
+	// printk("Forcing re-check\n");
+
+	// In theory, this should could be turned on whenever we detect the solar power present
+
+	// bq.enable_mppt()
+	// if (i2c_reg_write_byte_dt(&pmic_i2c, REG_MPPT_Control, 0x4B)) {
+	// 	goto failed;
+	// }
+
+	// printk("Enabled MPPT\n");
+
+	// bq.configure_control5()
+	// disable EN_EXTILIM, enable EN_IBAT
+	if (i2c_reg_write_byte_dt(&pmic_i2c, REG_Charger_Control_5, 0x3c)) {
+		goto failed;
+	}
+	
+	if (i2c_reg_read_byte_dt(&pmic_i2c, REG_Input_Voltage_Limit, &byte_val)) {
+		goto failed;
+	}
+
+	printk("Disable IINDPM limit from external pin\n");
+	printk("Enable IBAT discharge sensing\n");
+
+	// bq.config_vindpm(3600)
+	if (i2c_reg_write_byte_dt(&pmic_i2c, REG_Input_Voltage_Limit, 36)) {
+		goto failed;
+	}
+	
+	if (i2c_reg_read_byte_dt(&pmic_i2c, REG_Input_Voltage_Limit, &byte_val)) {
+		goto failed;
+	}
+
+	printk("Input Voltage Limit = %d mV\n", byte_val * 100);
+
+ 	// bq.config_iindpm(100)
+	if (i2c_reg_write_word_dt(&pmic_i2c, REG_Input_Current_Limit, 50)) {
+		goto failed;
+	}
+	
+	if (i2c_reg_read_word_dt(&pmic_i2c, REG_Input_Current_Limit, &word_val)) {
+		goto failed;
+	}
+
+	printk("Input Current Limit = %d mA\n", word_val * 10);
+
 	return 0;
+failed:
+	printk("ERROR DURING BQ25 COMMANDS\n");
+	return 1;
 }
 
 int main(void)
@@ -199,7 +373,8 @@ int main(void)
 	test_pmic_i2c();
 	test_sbus_i2c();
 
-	program_default_params();
+	// assumes a ~ 1500 mA lifepo4 cell
+	program_charge_params();
 
 	enable_charging();
 
